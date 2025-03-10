ea34688f7ddd87df1c29e40e2ef013d1a1ff155e
christianmennerich <177663202+christianmennerich@users.noreply.github.com>
2025-03-10 12:22:23 +0000

    DL-15631 - Updates dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -16,20 +16,20 @@
 import sbt.*
 
 object AppDependencies {
-  val bootstrapVersion = "9.1.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"        % "2.2.0",
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "com.github.fge"    %  "json-schema-validator"     % "2.2.14",
-    "org.typelevel"     %% "cats-core"                 % "2.12.0",
-    "com.chuusai"       %% "shapeless"                 % "2.3.12"
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"        % "2.6.0",
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapVersion,
+    "com.github.java-json-tools"   %% "json-schema-validator"     % "2.2.14",
+    "org.typelevel"                %% "cats-core"                 % "2.13.0",
+    "com.chuusai"                  %% "shapeless"                 % "2.3.13"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "7.0.1",
-    "org.scalamock"          %% "scalamock"              % "6.0.0",
+    "org.scalamock"          %% "scalamock"              % "6.2.0",
     "org.scalatestplus"      %% "scalacheck-1-18"        % "3.2.19.0"
   ).map(_ % Test)
 
